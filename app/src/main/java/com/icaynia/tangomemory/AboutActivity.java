ecfb0220f82a967de32f8074bfa4477a7aad2f0a
icaynia <icaynia@icaynia.com>
2016-12-12 14:59:08 +0900

    about activity

--- app/src/main/java/com/icaynia/tangomemory/AboutActivity.java
+++ app/src/main/java/com/icaynia/tangomemory/AboutActivity.java
@@ -1,7 +1,13 @@
 package com.icaynia.tangomemory;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by icaynia on 2016. 12. 10..
@@ -13,5 +19,24 @@ public class AboutActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView createdBy = (TextView) findViewById(R.id.createdby);
+        createdBy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://icaynia.com/"));
+                startActivity(intent);
+            }
+        });
+
+        TextView opensource = (TextView) findViewById(R.id.github);
+        opensource.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/icaynia/tangoii"));
+                startActivity(intent);
+            }
+        });
+
     }
 }
